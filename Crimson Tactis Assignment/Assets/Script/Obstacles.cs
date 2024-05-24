0e887a741f0033f39e831f0d0555391767b95b4b
KB <79893099+KB-112@users.noreply.github.com>
2024-05-24 20:23:28 +0530

    Obstacle SpawningDone

--- Crimson Tactis Assignment/Assets/Script/Obstacles.cs
+++ Crimson Tactis Assignment/Assets/Script/Obstacles.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+
+using TMPro;
+
 using UnityEngine;
 
 public class Obstacles : MonoBehaviour
@@ -11,7 +11,7 @@ public class Obstacles : MonoBehaviour
     public GameObject tile2D;
     GridGeneration gridGeneration;
     public GameObject parentObject;
-  
+     TextMeshProUGUI coordinatesText;
     
 
   
@@ -29,10 +29,12 @@ public class Obstacles : MonoBehaviour
 
     private void GridObstacleDeployment()
     {
-        for (int i = 0; i <= gridGeneration.gridSize.x; i++)
+        for (int i = 0; i < gridGeneration.gridSize.x; i++)
         {
-            for (int j = 0; j <= gridGeneration.gridSize.y; j++)
+            for (int j = 0; j < gridGeneration.gridSize.y; j++)
             {
+               coordinatesText= tile2D.GetComponentInChildren<TextMeshProUGUI>();
+                coordinatesText.text = $"{i},{j}" ;
                 Vector3 tilePosition = new Vector3(-842 + (i * distance), -452 + (j * distance), 0);
                 GameObject tile = Instantiate(tile2D, parentObject.transform, false);
                 RectTransform rectTransform = tile.GetComponent<RectTransform>();
